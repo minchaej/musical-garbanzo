07555b384ffcfdf0bc4427701a3ed5952dbeccbc
Minchae Jeong <minchaej@gmail.com>
2021-06-21 14:47:18 +0900

    Fibonacci with memoization

--- ClimbingStairs.kt
+++ ClimbingStairs.kt
@@ -17,4 +17,49 @@ class ClimbingStairs {
         return second
     }
 
+    companion object {
+        var nodeCounter = 0
+    }
+
+    fun calculateFibonacci(n: Int): Int {
+        val memo = IntArray(n)
+        val ans = getFibonacciWithMemo(n, memo)
+        memo.forEach { print("${it},") }
+        println("")
+        return ans
+//        return getFibonacciWithMemo(n, memo)
+//        return getFibonacciBrute(n)
+    }
+
+    private fun getFibonacciBrute(n: Int): Int {
+        if (n < 2) {
+            return n
+        }
+        nodeCounter += 2
+        return getFibonacciBrute(n - 1) + getFibonacciBrute( n - 2)
+    }
+
+    private fun getFibonacciWithMemo(n: Int, memo: IntArray): Int {
+        if (n < 2) {
+            return n
+        }
+        val lhs = if (memo[n - 1] == 0) {
+            nodeCounter++
+            memo[n - 1] = getFibonacciWithMemo(n - 1, memo)
+            memo[n - 1]
+        } else {
+            memo[n - 1]
+        }
+
+        val rhs = if (memo[n - 2] == 0) {
+            nodeCounter++
+            memo[n - 2]= getFibonacciWithMemo(n - 2, memo)
+            memo[n - 2]
+        } else {
+            memo[n - 2]
+        }
+
+        return lhs + rhs
+    }
+
 }
